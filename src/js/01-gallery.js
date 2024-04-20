2748c612e89be0f9cee3ed1dcde7d2901a12b87a
lukamatcha12 <148770204+lukamatcha12@users.noreply.github.com>
2024-04-20 20:19:11 +0800

    gallery done

--- src/js/01-gallery.js
+++ src/js/01-gallery.js
@@ -1,5 +1,35 @@
-// Add imports above this line
-import { galleryItems } from './gallery-items';
+import SimpleLightbox from 'simplelightbox';
+import 'simpleLightbox/dist/simple-lightbox.min.css';
+
 // Change code below this line
+import { galleryItems } from './gallery-items.js';
+// Change code below this line
+
+const galleryContainer = document.querySelector("ul.gallery");
+
+function createGalleryItem(array) {
+    return array
+      .map(({ preview, original, description }) => {
+        return `
+     <a class="gallery__item" href="${original}">
+        <img class="gallery__image" src="${preview}" alt="${description}" />
+     </a>
+  `;
+      })
+      .join("");
+  }
+
+  const photosMarkup = createGalleryItem(galleryItems);
+
+  galleryContainer.insertAdjacentHTML("beforeend", photosMarkup);
+
+
+  const galleryHandler = new SimpleLightbox(".gallery a", {
+    captionsData: "alt",
+    captionDelay: 250,
+  });
+
+  galleryHandler.on("show.simplelightbox");
+
 
 console.log(galleryItems);
