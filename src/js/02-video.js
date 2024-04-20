0e06f48beeed60c53d4e7436fc42c7bd36e7d822
lukamatcha12 <148770204+lukamatcha12@users.noreply.github.com>
2024-04-20 20:24:22 +0800

    done?

--- src/js/02-video.js
+++ src/js/02-video.js
@@ -1,1 +1,27 @@
+import Player from "@vimeo/player";
+import throttle from "lodash.throttle";
+const iframe = document.querySelector('iframe');
+const player = new Player(iframe);
 
+const timeFunction= (data)=>{
+let resumeTime= data.seconds;
+localStorage.setItem('videoplayer-current-time',resumeTime);
+}
+
+player.on('timeupdate', throttle(timeFunction,1000));
+let timeResume=localStorage.getItem('videoplayer-current-time');
+
+console.log(timeResume);
+player.setCurrentTime(timeResume).then(function(seconds) {
+    // seconds = the actual time that the player seeked to
+}).catch(function(error) {
+    switch (error.name) {
+        case 'RangeError':
+            // the time was less than 0 or greater than the video’s duration
+            break;
+
+        default:
+            // some other error occurred
+            break;
+    }
+});
--- src/js/03-feedback.js
+++ src/js/03-feedback.js
@@ -1,1 +1,46 @@
+import throttle from "lodash.throttle";
 
+let form=document.querySelector('form.feedback-form');
+let emailEl=document.querySelector('label [name = "email"]');
+let mesageEl= document.querySelector('label [name="message"]');
+
+const STORAGE_KEY ='feddback-form-state';
+
+function onPageReload() {
+    const savedMessage= JSON.parse(localStorage.getItem(STORAGE_KEY));
+    if(savedMessage){
+        emailEl.value=savedMessage.email;
+        mesageEl.value=savedMessage.message;
+
+    }
+}
+
+onPageReload()
+
+form.addEventListener('input', throttle(onFormInput,500));
+
+function onFormInput(){
+    const email =emailEl.value;
+    const message = mesageEl.value;
+
+    const formData={
+        email,
+        message,
+    };
+    localStorage.setItem(STORAGE_KEY,JSON.stringify(formData));
+}
+
+function onFormSubmit(e){
+e.preventDefault();
+const email =emailEl.value;
+    const message = mesageEl.value;
+
+    if (email =="" || message ==""){
+        alert('Please fill up both fields');
+        form.reset();
+        return;
+    }
+    form.reset();
+    localStorage.removeItem(STORAGE_KEY);
+}
+document.addEventListener('submit', onFormSubmit);
